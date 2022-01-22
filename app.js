9b089fd6540f9cdd4abdccaf98cf33f0cde4ff1e
Wyatt C <wcastellan@yahoo.com>
2022-01-22 11:34:00 -0500

    have the database up and running

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const session = require('express-session');
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize');
+
 var imageRouter = require('./routes/image-route');
 app.use('/', imageRouter);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
