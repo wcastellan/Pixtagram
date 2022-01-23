ff1bf99eada2953645dac16a694fdd915cd7f54a
Wyatt C <wcastellan@yahoo.com>
2022-01-23 10:36:08 -0500

    update the javascript upload.js file and finish the controller-api folder

--- public/javascript/upload.js
+++ public/javascript/upload.js
@@ -1,7 +1,8 @@
 async function uploadClickHandler(event) {
     event.preventDefault();
 
-
+    var image = document.getElementById('output');
+    image.src = URL.createObjectURL(event.target.files[0]);
 }
 
 document.querySelector('.upload-btn').addEventListener('click', uploadClickHandler);
