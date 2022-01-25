9751680efe25c590471580063a36965a6db0acb9
Wyatt C <wcastellan@yahoo.com>
2022-01-24 22:19:45 -0500

    able to POST an image on insomnia and install cloudify

--- public/javascript/upload.js
+++
@@ -1,8 +0,0 @@
-async function uploadClickHandler(event) {
-    event.preventDefault();
-
-    var image = document.getElementById('output');
-    image.src = URL.createObjectURL(event.target.files[0]);
-}
-
-document.querySelector('.upload-btn').addEventListener('click', uploadClickHandler);
