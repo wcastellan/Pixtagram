152c917da3fa41c686374842e77a6a28f97175f2
Wyatt C <wcastellan@yahoo.com>
2022-01-26 11:54:59 -0500

    update upload-route, add upload.js to public/javascript

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
+const uploadRoutes = require('./upload-route.js');
 
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
+router.use('/upload', uploadRoutes);
 
 module.exports = router;
