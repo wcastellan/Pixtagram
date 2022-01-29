0abc277cbe6bfcb6e81b0d6ded732127d332e0cc
Wyatt C <wcastellan@yahoo.com>
2022-01-29 11:05:43 -0500

    troubleshooting the upload button

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./models/Upload'));
+app.use('./models/Upload', Upload);
 
 app.use(require('./controllers/'));
 
--- public/javascript/upload.js
+++ public/javascript/upload.js
@@ -1,32 +1,32 @@
-var document = require('global/document');
+// var document = require('global/document');
 
-document.addEventListener('DOMContentloaded', async () => {
-  const response = await fetch('/api/upload');
-  const data = await response.json();
+// document.addEventListener('DOMContentloaded', async () => {
+//   const response = await fetch('/api/upload');
+//   const data = await response.json();
 
-  const options = {
-    cloudName: data.cloudname,
-    apiKey: data.apikey,
-    uploadSignatureTimestamp: data.timestamp,
-    uploadSignature: data.signature,
-    cropping: false,
-    folder: 'sample'
-  }
+//   const options = {
+//     cloudName: data.cloudname,
+//     apiKey: data.apikey,
+//     uploadSignatureTimestamp: data.timestamp,
+//     uploadSignature: data.signature,
+//     cropping: false,
+//     folder: 'sample'
+//   }
 
-  const processResults = (error, result) => {
-    if (!error && result && result.event === 'success') {
-      console.log(result)
+//   const processResults = (error, result) => {
+//     if (!error && result && result.event === 'success') {
+//       console.log(result)
 
-      var str = JSON.stringify(result, null, 4);
-      document.getElementById('sampledata').innerHTML += str;
-    }
-  }
+//       var str = JSON.stringify(result, null, 4);
+//       document.getElementById('sampledata').innerHTML += str;
+//     }
+//   }
 
-  const myWidget = window.cloudinary.createUploadWidget(
-    options,
-    processResults
-  )
-  document
-    .getElementById('upload')
-    .addEventListener('click', () => myWidget.open(), false)
-})
+//   const myWidget = window.cloudinary.createUploadWidget(
+//     options,
+//     processResults
+//   )
+//   document
+//     .getElementById('#upload_widget')
+//     .addEventListener('click', () => myWidget.open(), false)
+// })
