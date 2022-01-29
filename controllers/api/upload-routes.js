b64d7532cb14a4310f8b58f8dac55dda0ac669cc
Wyatt C <wcastellan@yahoo.com>
2022-01-29 09:49:15 -0500

    add gloudinary backend

--- controllers/api/upload-routes.js
+++ controllers/api/upload-routes.js
@@ -1,74 +1,21 @@
-const router = require('express').Router();
-const Upload = require('../../models/Upload');
-const cloudinary = require('../../utils/cloudinary');
-const upload = require('../../utils/multer');
+const express = require('express');
+const router = express.Router();
+const signature = require('../../models/Upload');
+require('../../utils/cloudinary');
 
-// post request
-router.post('/', upload.single('image'), async (req, res) => {
-    try {
-        // upload image to cloudinary
-        const result = await cloudinary.uploader.upload(req.file.path)
-        // create new upload
-        console.log(result)
-        
-        let upload = new Upload({
-            name: req.body.name,
-            avatar: result.secure_url,
-            cloudinary_id: result.public_id
-        });
-        // save upload
-        await upload.save();
-        res.json(upload);
-    } catch (err) {
-        console.log(err)
-    }
-});
+const cloudinary = require('cloudinary').v2
+const cloudName = cloudinary.config().cloud_name;
+const apiKey = cloudinary.config().api_key;
 
-router.get('/:id', async (req, res) => {
-    try {
-        let upload = await Upload.findOne({
-            where: {
-                id: req.params.id
-            }
-        });
-
-        res.json(upload);
-    } catch (err) {
-        console.log(err);
-    }
-});
-
-router.delete('/:id', async (req, res) => {
-    try {
-        // find upload by id
-        let upload = await Upload.findById(req.params.id);
-        // delete image from cloudinary
-        await cloudinary.uploader.destroy(upload.cloudinary_id);
-        res.json(upload);
-    } catch (err) {
-        console.log(err);
-    }
-});
-
-router.put('/:id', upload.single('image'), async(req, res) => {
-    try {
-        let upload = await Upload.findById(req.params.id);
-        // delete image from cloudinary
-        await cloudinary.uploader.destroy(upload.cloudinary_id);
-        // upload image to cloudinary
-        const result = await cloudinary.uploader.upload(req.file.path);
-        const data = {
-            name: req.body.name || upload.name,
-            avatar: result.secure_url || upload.avatar,
-            cloudinary_id: result.public_id || upload.cloudinary_id
-        };
-        upload = await Upload.findByIdAndUpdate(req.params.id, data, {
-            new: true
-        });
-        res.json(upload);
-    } catch (err) {
-        console.log(err);
-    }
-});
+// using this API should require authentication
+router.get('/', function(req, res, next) {
+    const sig = signature.Upload()
+    res.json({
+        signature: sig.signature,
+        timestamp: sig.timestamp,
+        cloudname: cloudName,
+        apikey: apiKey
+    })
+})
 
 module.exports = router;
